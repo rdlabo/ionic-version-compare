4b38ebdfcc22698df323a22de3b7c31ade0f3135
榊原昌彦 <sakakibara@rdlabo.jp>
2020-04-12 17:09:13 +0900

    chore

--- packages/ion5-ng9_Ivy/src/app/app.module.ts
+++ packages/ion5-ng9_Ivy/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: APP_BASE_HREF, useValue: '/ion5-ng9_Ivy' }
   ],
   bootstrap: [AppComponent]
 })
